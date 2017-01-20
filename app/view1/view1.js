6f6f13123a2e6c009aa72f2d32ecd2b7bb94574f
Gleb Danichev <yonesko@gmail.com>
2017-01-20 22:56:38 +0300

    rework quota limit handling

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -8,19 +8,18 @@ angular.module('myApp.view1', ['ngRoute'])
             controller: 'View1Ctrl'
         });
     }])
-    .controller('View1Ctrl', ['$scope', 'NgMap', '$timeout',
-        function ($scope, NgMap, $timeout) {
+    .controller('View1Ctrl', ['$scope', 'NgMap', '$timeout', '$log',
+        function ($scope, NgMap, $timeout, $log) {
             const
                 kilo = 1000,
-                second_ms = 1000,
                 radar_radius_m = 20 * kilo,
-                radar_second_qouta = 2,
                 centralRussia = {lat: 57.452744, lng: 33.238945},
                 eestiCenter = new google.maps.LatLng(58.85, 25.64),
                 tallin = {lat: 59.41, lng: 24.75},
                 tartu = {lat: 58.36, lng: 26.72},
                 reutov = {lat: 55.759970, lng: 37.859058},
                 places_specification_per_point_quota = 1,
+                places_request_delay_ms = 20,
                 supported_place_types = ['museum', 'bar', 'restaurant', 'casino'];
 
             function distance(route) {
@@ -31,66 +30,16 @@ angular.module('myApp.view1', ['ngRoute'])
                 return sum;
             }
 
-            function radar(center) {
-                return function () {
-                    if (debug) {
-                        new google.maps.Circle({
-                            strokeColor: '#FF0000',
-                            strokeOpacity: 0.8,
-                            strokeWeight: 0,
-                            fillColor: '#FF0000',
-                            fillOpacity: 0.35,
-                            map: mapInstance,
-                            center: center,
-                            radius: radar_radius_m
-                        });
-                    }
-
-                    placesService.radarSearch({
-                            location: center,
-                            radius: radar_radius_m,
-                            type: $scope.placeTypeForSearch
-                        }, function (places, status) {
-                            if (status !== google.maps.places.PlacesServiceStatus.OK) {
-                                console.log('findPlacesAlongRoute ' + status);
-                                return;
-                            }
-
-                            var delay_ms = (second_ms / radar_second_qouta) * 1.1;
-                            for (var i = 0, place; i < places.length && i < places_specification_per_point_quota; i++) {
-                                place = places[i];
-
-                                if (isPlaceAlreadyFound(place))
-                                    continue;
-
-                                $timeout(function (place) {
-                                    return function () {
-                                        placesService.getDetails(place, function (placeDetail, status) {
-                                            if (status !== google.maps.places.PlacesServiceStatus.OK) {
-                                                console.log('getDetails ' + status);
-                                                return;
-                                            }
-                                            addMarkerForPlace(placeDetail);
-                                            $scope.foundPlaces.push(placeDetail);
-                                            // console.log($scope.foundPlaces)
-                                        })
-                                    };
-                                }(place), i * delay_ms);
-                            }
-                        }
-                    )
-                }
-            }
-
             function isPlaceAlreadyFound(place) {
-                for (var i in $scope.foundPlaces)
+                for (var i = 0; i < $scope.foundPlaces.length; i++)
                     if ($scope.foundPlaces[i].place_id == place.place_id)
                         return true;
                 return false;
             }
 
             function addMarkerForPlace(place) {
-                var marker = new google.maps.Marker({
+                //mark place on the map
+                place.marker = new google.maps.Marker({
                     map: mapInstance,
                     position: place.geometry.location,
                     icon: {
@@ -101,22 +50,51 @@ angular.module('myApp.view1', ['ngRoute'])
                     title: place.name,
                     clickable: false
                 });
-                place.denotionMarker = new google.maps.Marker({
-                    map: mapInstance,
-                    position: place.geometry.location,
-                    visible: false,
-                    animation: google.maps.Animation.DROP,
-                    clickable: false
-                });
-
+                //add place name on the map
                 new MapLabel({
-                    text: place.name,
+                    text: place.name.length > 25 ? place.name.substring(0, 25) + '...' : place.name,
                     position: place.geometry.location,
                     map: mapInstance,
                     align: 'right'
                 });
-
             }
+
+            function processPlacesRequestQueue() {
+                var request = placesRequestQueue.shift();
+                if (request)
+                    request();
+                else
+                    clearInterval(processPlacesRequestIntervalId);
+            }
+
+            function addToPlacesRequesQueue(request) {
+                placesRequestQueue.push(request);
+                processPlacesRequestIntervalId = setInterval(processPlacesRequestQueue, places_request_delay_ms);
+            }
+
+            $scope.cancelVisit = function (place) {
+                console.log('cancel ' + place.name);
+            };
+
+            //add a waypoint and marker for it
+            $scope.visit = function (place) {
+                console.log('visit ' + place.name);
+                // var ind;
+                // if ((ind = waypoitns.indexOf(place.geometry.location)) >= 0) {
+                //     console.log('remove')
+                //     waypoitns.splice(ind, 1);
+                //     place.visitMarker.setMap(null);
+                //     place.visitMarker = null;
+                // }
+                //
+                // place.visitMarker = new google.maps.Marker({
+                //     position: place.geometry.location,
+                //     map: mapInstance,
+                //     label: String(waypoitns.length + 1)
+                // });
+                // waypoitns.splice(1, 0, place.geometry.location);
+            };
+
 
             $scope.findPlacesAlongRoute = function () {
                 if (!route)
@@ -124,18 +102,60 @@ angular.module('myApp.view1', ['ngRoute'])
 
                 var dist = distance(route);
                 var step = Math.ceil((2 * radar_radius_m) / (dist / route.overview_path.length));
-                var delay_ms = (second_ms / radar_second_qouta) * 1.1;
 
-                for (var i = 0, cou = 0; i < route.overview_path.length; i += step, cou++) {
-                    $timeout(radar(route.overview_path[i]), cou * delay_ms);
+                for (var i = 0; i < route.overview_path.length; i += step) {
+                    if (debug) {
+                        new google.maps.Circle({
+                            strokeColor: '#FF0000',
+                            strokeOpacity: 0.8,
+                            strokeWeight: 0,
+                            fillColor: '#FF0000',
+                            fillOpacity: 0.35,
+                            map: mapInstance,
+                            center: route.overview_path[i],
+                            radius: radar_radius_m
+                        });
+                    }
+
+                    addToPlacesRequesQueue((function (center) {
+                        return function () {
+                            placesService.radarSearch({
+                                location: center,
+                                radius: radar_radius_m,
+                                type: $scope.placeTypeForSearch
+                            }, function (places, status) {
+                                if (status !== google.maps.places.PlacesServiceStatus.OK) {
+                                    $log.error('findPlacesAlongRoute radarSearch ' + status);
+                                    return;
+                                }
+
+                                for (var i = 0, place; i < places.length && i < places_specification_per_point_quota; i++) {
+                                    place = places[i];
+
+                                    if (isPlaceAlreadyFound(place))
+                                        continue;
+
+                                    addToPlacesRequesQueue((function (place) {
+                                        return function () {
+                                            placesService.getDetails(place, function (placeDetail, status) {
+                                                if (status !== google.maps.places.PlacesServiceStatus.OK) {
+                                                    $log.error('findPlacesAlongRoute getDetails ' + status);
+                                                    return;
+                                                }
+                                                addMarkerForPlace(placeDetail);
+                                                $scope.foundPlaces.push(placeDetail);
+                                                $scope.$apply();
+                                            });
+                                        }
+                                    })(place));
+                                }
+                            });
+                        }
+                    })(route.overview_path[i]));
                 }
-                $timeout(radar(route.overview_path[route.overview_path.length - 1]), cou * delay_ms);
             };
 
             $scope.buildRoute = function () {
-                if (waypoitns.length == 0 || route)
-                    return;
-
                 $scope.distance = 0;
 
                 var interpoints = [];
@@ -148,7 +168,8 @@ angular.module('myApp.view1', ['ngRoute'])
                     origin: waypoitns[0],
                     waypoints: interpoints,
                     destination: waypoitns[waypoitns.length - 1],
-                    travelMode: google.maps.TravelMode.DRIVING
+                    travelMode: google.maps.TravelMode.DRIVING,
+                    optimizeWaypoints: true
                 }, function (response, status) {
                     if (status === google.maps.DirectionsStatus.OK) {
                         route = response.routes[0];
@@ -156,26 +177,27 @@ angular.module('myApp.view1', ['ngRoute'])
                         $scope.$apply();
                         directionsDisplay.setDirections(response);
                     } else {
-                        window.alert('Directions request failed due to ' + status);
+                        $log.error('Directions request failed due to ' + status);
                     }
                 });
             };
 
+
             $scope.markerDrawed = function (marker) {
+                marker.setLabel(String(waypoitns.length + 1));
                 waypoitns.push(marker.getPosition());
             };
             $scope.test = function () {
                 waypoitns.push((new google.maps.Marker({
                     position: tartu,
-                    map: mapInstance
+                    map: mapInstance,
+                    label: String(waypoitns.length + 1)
                 })).getPosition());
                 waypoitns.push((new google.maps.Marker({
                     position: tallin,
-                    map: mapInstance
+                    map: mapInstance,
+                    label: String(waypoitns.length + 1)
                 })).getPosition());
-
-                // $scope.buildRoute();
-                // $scope.findPlacesAlongRoute();
             };
 
             $scope.foundPlaces = [];
@@ -183,13 +205,16 @@ angular.module('myApp.view1', ['ngRoute'])
             $scope.placeTypeForSearch = supported_place_types[0];
             $scope.supported_place_types = supported_place_types;
 
-            var placesService;
             var directionsService = new google.maps.DirectionsService;
             var directionsDisplay = new google.maps.DirectionsRenderer({suppressMarkers: true});
             var waypoitns = [];
             var route;
             var mapInstance;
             var debug = false;
+
+            var placesService;
+            var placesRequestQueue = [];
+            var processPlacesRequestIntervalId;
 
             NgMap.getMap().then(function (map) {
                 mapInstance = map;
